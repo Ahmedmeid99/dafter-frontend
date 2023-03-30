45f8585d82553aa85af0c3ddae7173fa0647c1dc
ahmedmeid99 <you@example.com>
2023-03-30 17:11:49 +0200

    new-hosting

--- src/Api/variables.js
+++ src/Api/variables.js
@@ -1,3 +1,3 @@
-/* export const URL = "https://dafter-backend-production.up.railway.app" */ // => old hosting
-export const URL = "https://dafter-backend.onrender.com"
+export const URL = "https://dafter-backend-production.up.railway.app" // => old hosting
+// export const URL = "https://dafter-backend.onrender.com"
 export const token = localStorage.getItem('token')
